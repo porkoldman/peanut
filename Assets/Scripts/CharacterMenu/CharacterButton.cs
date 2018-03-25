75c4ab26250992c13c73d9460992a30945fa4d65
porkoldman <gamedesigner555@gmail.com>
2018-03-25 15:23:26 +0800

    [ADD]Character toggle trigger

--- Assets/Scripts/CharacterMenu/CharacterButton.cs
+++ Assets/Scripts/CharacterMenu/CharacterButton.cs
@@ -9,6 +9,8 @@ public class CharacterButton : MonoBehaviour {
 	public GameObject selectedEffect;
 	public GameObject infoButton;
 
+	private PlayCharacterPanelController playCharacterPanelController;
+
 	private bool isSelected;
 
 	private CharacterData characterData;
@@ -19,21 +21,31 @@ public class CharacterButton : MonoBehaviour {
 		characterData = data;
 	}
 
+
+	public bool IsSelected() {
+		return isSelected;
+	}
+
 	public void ClickTrigger() {
 		if (isSelected == true) {
 			isSelected = false;
 			infoButton.SetActive (false);
 			selectedEffect.SetActive (false);
 		} else {
+			playCharacterPanelController.UnSelectCurrentCharacter ();
 			isSelected = true;
 			infoButton.SetActive (true);
 			selectedEffect.SetActive (true);
+			playCharacterPanelController.SetCurrentSelectedCharacter (this);
 		}
 	}
 
 	// Use this for initialization
 	void Start () {
-		
+		isSelected = false;
+		playCharacterPanelController = FindObjectOfType<PlayCharacterPanelController> ();
+
+
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/CharacterMenu/PlayCharacterPanelController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayCharacterPanelController : MonoBehaviour {
+
+	private int currentDeck = 0;
+
+	public CharacterButton currentSelectedCharacter;
+
+
+	public void UnSelectCurrentCharacter() {
+		if (currentSelectedCharacter == null) {
+			return;
+		}
+		if (currentSelectedCharacter.IsSelected () == true) {
+			currentSelectedCharacter.ClickTrigger ();
+		}
+	}
+
+	public void SelectCurrentCharacter() {
+		if (currentSelectedCharacter == null) {
+			return;
+		}
+		if (currentSelectedCharacter.IsSelected () == false) {
+			currentSelectedCharacter.ClickTrigger ();
+		}
+	}
+
+	public void SetCurrentSelectedCharacter(CharacterButton obj) {
+		currentSelectedCharacter = obj;
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
