f9dd030c74f3aa6dce3d0faf097ed24ec38c85fa
porkoldman <gamedesigner555@gmail.com>
2018-03-27 20:46:34 +0800

    [ADD]write comment to switch deck button

--- Assets/Scripts/CharacterMenu/SwitchDackButton.cs
+++ Assets/Scripts/CharacterMenu/SwitchDackButton.cs
@@ -5,21 +5,28 @@ using UnityEngine.UI;
 
 public class SwitchDackButton : MonoBehaviour
 {
+	// the deck button's index
 	public int deckIndex;
 
+	// the controller to handle character menu scene.
 	private CharacterMenuController characterMenuController;
+
+	// the controller to handle all SwitchDeckButton.
 	private SwitchDeckButtonController switchDeckButtonController;
 
+	// init when scene start.
 	void Start () {
 		characterMenuController = FindObjectOfType<CharacterMenuController> ();
 		switchDeckButtonController = FindObjectOfType<SwitchDeckButtonController> ();
 	}
 
+	// make button to unhightlight status.
 	public void UnHightLight() {
 		var block005Sprite = GameObject.Find ("/Canvas/Material/block_005").GetComponent<Image> ().sprite;
 		gameObject.GetComponent<Image> ().sprite = block005Sprite;
 	}
 
+	// make button to hightlight status.
 	public void HightLight() {
 		if (switchDeckButtonController.selectedIndex == deckIndex) {
 			return;
--- Assets/Scripts/CharacterMenu/SwitchDeckButtonController.cs
+++ Assets/Scripts/CharacterMenu/SwitchDeckButtonController.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class SwitchDeckButtonController : MonoBehaviour
 {
+	// record all buttons that need to be controlled 
 	public SwitchDackButton[] allSwitchDeckButton;
 
+	// record button's index that is selected now.
 	public int selectedIndex = 0 ;
-
 }
